e7e8eed439c4aa3bcb9da0355d88ded682a3a15d
Chirag Rangras <chiragrangras012@gmail.com>
2024-10-11 18:27:21 +0530

    modified file

--- angular_tutorial/src/app/components/data-binding/data-binding.component.ts
+++ angular_tutorial/src/app/components/data-binding/data-binding.component.ts
@@ -1,9 +1,10 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.css',
 })
@@ -11,16 +12,23 @@ export class DataBindingComponent {
   //string, number, boolean, date
 
   courseName: string = 'Angular 18';
-
+  stateName: string = 'Gujarat';
   inputType = 'radio';
-
   myClassName: string = 'bg-primary';
-
   rollNo: number = 123;
-
-  isIndian: boolean = false;
-
+  isIndian: boolean = true;
   currentDate: Date = new Date();
 
+  firstName = signal('chirag rangras');
+
   constructor() {}
+
+  changeCourseName() {
+    this.courseName = 'React JS';
+    this.firstName.set('Vishal Rangras');
+  }
+
+  showAlert(message: string) {
+    alert(message);
+  }
 }
