972cb188deb6592592cdd72042f895997dde552d
Chirag Rangras <chiragrangras012@gmail.com>
2024-10-13 17:27:53 +0530

    modified file

--- angular_tutorial/src/app/components/directive/structural-dir/structural-dir.component.ts
+++ angular_tutorial/src/app/components/directive/structural-dir/structural-dir.component.ts
@@ -1,16 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-structural-dir',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './structural-dir.component.html',
   styleUrl: './structural-dir.component.css',
 })
 export class StructuralDirComponent {
   isDiv1Visiable: boolean = true;
   isDiv2Visiable: boolean = true;
+
+  num1: string = '';
+  num2: string = '';
+
+  isActive: boolean = false;
 
   showDiv1() {
     this.isDiv1Visiable = true;
