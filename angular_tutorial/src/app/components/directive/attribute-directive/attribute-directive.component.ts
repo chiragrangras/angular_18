0d78ce3f4bc3049d61221c95892ef8e31b8573aa
Chirag Rangras <chiragrangras012@gmail.com>
2024-10-15 12:43:59 +0530

    modified file

--- angular_tutorial/src/app/components/directive/attribute-directive/attribute-directive.component.ts
+++ angular_tutorial/src/app/components/directive/attribute-directive/attribute-directive.component.ts
@@ -16,13 +16,63 @@ export class AttributeDirectiveComponent {
   num2: string = '';
   isActive: boolean = false;
 
+  customerStyle: any = {
+    color: 'white',
+    background: 'red',
+    width: '200px',
+    height: '200px',
+    'border-radius': '50%',
+  };
+
   studentList: any[] = [
-    { studentId: 13, totalMarks:23, gender:'male', name: 'AAA', city: 'Gujarat', isActive: false },
-    { studentId: 25, totalMarks:53, gender:'female', name: 'BBB', city: 'Ajmer', isActive: false },
-    { studentId: 53, totalMarks:67, gender:'male', name: 'CCC', city: 'Pune', isActive: true },
-    { studentId: 44, totalMarks:98, gender:'female', name: 'DDD', city: 'Nagpur', isActive: false },
-    { studentId: 52, totalMarks:20, gender:'male', name: 'EEE', city: 'Delhi', isActive: false },
-    { studentId: 64, totalMarks:50, gender:'male', name: 'FFF', city: 'Mumbai', isActive: true },
+    {
+      studentId: 13,
+      totalMarks: 23,
+      gender: 'male',
+      name: 'AAA',
+      city: 'Gujarat',
+      isActive: false,
+    },
+    {
+      studentId: 25,
+      totalMarks: 53,
+      gender: 'female',
+      name: 'BBB',
+      city: 'Ajmer',
+      isActive: false,
+    },
+    {
+      studentId: 53,
+      totalMarks: 67,
+      gender: 'male',
+      name: 'CCC',
+      city: 'Pune',
+      isActive: true,
+    },
+    {
+      studentId: 44,
+      totalMarks: 98,
+      gender: 'female',
+      name: 'DDD',
+      city: 'Nagpur',
+      isActive: false,
+    },
+    {
+      studentId: 52,
+      totalMarks: 20,
+      gender: 'male',
+      name: 'EEE',
+      city: 'Delhi',
+      isActive: false,
+    },
+    {
+      studentId: 64,
+      totalMarks: 50,
+      gender: 'male',
+      name: 'FFF',
+      city: 'Mumbai',
+      isActive: true,
+    },
   ];
 
   addRedClass() {
