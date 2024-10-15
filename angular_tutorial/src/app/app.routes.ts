52d9c253d962e096ebb5796876bb0241bc059ad8
Chirag Rangras <chiragrangras012@gmail.com>
2024-10-15 18:28:16 +0530

    modified file

--- angular_tutorial/src/app/app.routes.ts
+++ angular_tutorial/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { StructuralDirComponent } from './components/directive/structural-dir/structural-dir.component';
+import { AttributeDirectiveComponent } from './components/directive/attribute-directive/attribute-directive.component';
+import { IfelseComponent } from './components/controlFlow/ifelse/ifelse.component';
+import { ForComponent } from './components/controlFlow/for/for.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'add-emp',
+        component:AddEmployeeComponent
+    },
+    {
+        path:'data-binding',
+        component:DataBindingComponent
+    },
+    {
+        path:'emp-list',
+        component:EmployeeListComponent
+    },
+    {
+        path:'structural-dir',
+        component:StructuralDirComponent
+    },
+    {
+        path:'attribute-dir',
+        component:AttributeDirectiveComponent
+    },
+    {
+        path:'if-else',
+        component:IfelseComponent
+    },
+    {
+        path:'for',
+        component:ForComponent
+    }
+];
