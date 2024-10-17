ca62dcdb9447e637b6da8f9255e7c3978877c829
Chirag Rangras <chiragrangras012@gmail.com>
2024-10-17 18:25:45 +0530

    modified file

--- angular_tutorial/src/app/components/forms/template/template.component.ts
+++ angular_tutorial/src/app/components/forms/template/template.component.ts
@@ -1,21 +1,41 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './template.component.html',
   styleUrl: './template.component.css',
 })
 export class TemplateComponent {
   studentObj: any = {
-    firstName:'',
-    lastName:'',
-    userName:'',
-    city:'',
-    state:'',
-    zipCide:'',
-    isAcceptTerms:false
+    firstName: '',
+    lastName: '',
+    userName: '',
+    city: '',
+    state: '',
+    zipCode: '',
+    isAcceptTerms: false,
   };
+
+  formValue: any;
+
+  onSubmit() {
+    debugger;
+    this.formValue = this.studentObj;
+  }
+
+  resetForm() {
+    this.studentObj = {
+      firstName: '',
+      lastName: '',
+      userName: '',
+      city: '',
+      state: '',
+      zipCode: '',
+      isAcceptTerms: false,
+    };
+  }
 }
