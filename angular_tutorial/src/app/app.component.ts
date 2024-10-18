5e11dfec80f6533e1277d1386929b274944538b5
Chirag Rangras <chiragrangras012@gmail.com>
2024-10-18 16:56:48 +0530

    modified file

--- angular_tutorial/src/app/app.component.ts
+++ angular_tutorial/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,NavbarComponent],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
