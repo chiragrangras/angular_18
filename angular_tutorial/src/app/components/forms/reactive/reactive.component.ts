82a42a42e4c7d7581013cd8ea4f970e496b3b9b9
Chirag Rangras <chiragrangras012@gmail.com>
2024-10-18 18:13:47 +0530

    modified file

--- angular_tutorial/src/app/components/forms/reactive/reactive.component.ts
+++ angular_tutorial/src/app/components/forms/reactive/reactive.component.ts
@@ -5,28 +5,24 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 @Component({
   selector: 'app-reactive',
   standalone: true,
-  imports: [ReactiveFormsModule,JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './reactive.component.html',
-  styleUrl: './reactive.component.css'
+  styleUrl: './reactive.component.css',
 })
 export class ReactiveComponent {
-
-
   studentForm: FormGroup = new FormGroup({
-    firstName: new FormControl("",[Validators.required,Validators.minLength(4)]),
-    lastName: new FormControl(""),
-    userName: new FormControl("some@123",[Validators.email]),
-    city: new FormControl(""),
-    state: new FormControl(""),
-    zipCode: new FormControl(""),
-    isAcceptTerms: new FormControl("")
+    firstName: new FormControl('',[Validators.required,Validators.minLength(4)]),
+    lastName: new FormControl(''),
+    userName: new FormControl('',[Validators.email]),
+    city: new FormControl(''),
+    state: new FormControl(''),
+    zipCode: new FormControl(''),
+    isAcceptForm: new FormControl(''),
   });
 
-  formValue: any ;
+  formValue: any;
 
-  onSave( ) {
-    this.formValue =  this.studentForm.value;
+  onSave() {
+    this.formValue = this.studentForm.value;
   }
-
-
 }
